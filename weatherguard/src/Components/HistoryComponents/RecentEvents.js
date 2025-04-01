5b41380eff5ee5419564f1eece3bec1637330458
Ind3y <linderpreet26s@gmail.com>
2025-04-01 01:10:01 +0100

    Introduction of openMeteo API for free historical data

---
+++ weatherguard/src/Components/HistoryComponents/RecentEvents.js
@@ -0,0 +1,9 @@
+import React,{useState, useEffect} from "react";
+
+const RecentEvents = () => {
+    return (
+        <h2>The event</h2>
+    );
+};
+
+export default RecentEvents;
---
+++ weatherguard/src/Pages/HistoryPage.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useParams,useNavigate } from "react-router-dom";
+import '../CSS/History.css'
+import '../CSS/sidebar.css';
+import Sidebar from "../Components/Sidebar";
+import RecentEvents from "../Components/HistoryComponents/RecentEvents";
+
+const regions = [
+    { name: "Lonsdads", region: "Choose Region" },
+    { name: "Nottingham", region: "East Midlands" },
+    { name: "Birmingham", region: "West Midlands" },
+    { name: "Manchester", region: "North West" },
+    { name: "Liverpool", region: "North West" },
+    { name: "Leeds", region: "Yorkshire and the Humber" },
+    { name: "London", region: "London" },
+    { name: "Bristol", region: "South West" },
+    { name: "Tonbridge", region: "South East" },
+    { name: "Cardiff", region: "Wales" },
+    { name: "Edinburgh", region: "Scotland" },
+    { name: "Ballymena", region: "Northern Ireland" },
+];
+
+const History = () => {
+    const { city } = useParams(); 
+    const navigate = useNavigate();
+
+    const handleCityChange = async(e) => {
+        const selectedCity = e.target.value;
+        navigate(`/history/${selectedCity}`);
+    }
+
+    return(
+        <div className="history-page">
+            <Sidebar  city = {city}/>
+            <div className="main-content">
+                <header className="main-header">
+                    <h1>{city}</h1>
+                    <select onChange={handleCityChange} value={city}>
+                        {regions.map((region, index) => (
+                            <option key={index} value={region.name}>
+                                {region.region}
+                            </option>
+                        ))}
+                    </select>
+                </header>
+                <header>
+                    <h2>Weather history</h2>
+                </header>
+                <div className="weather-history">
+                    <h2>Recent Events</h2>
+                    <RecentEvents/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default History;
